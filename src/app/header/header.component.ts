0fb03d7ab734790846583ee95d3b0a4b519cdec1
Dhrubot <dhrubo.dh@gmail.com>
2021-02-13 00:56:40 -0500

    done with the user registration dialog and stepper need to have a confirmation page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CreateUserComponent } from '../users/user/create-user/create-user.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  title = 'student-opportunity-center';
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  onClickCreateUser(){
+    const dialogConfig = new MatDialogConfig() 
+    dialogConfig.width = '70%';
+    dialogConfig.height = '90%';
+
+    this.dialog.open(CreateUserComponent, dialogConfig)
   }
 
 }
