9723ae298074737ab86bea0a394a9b18302c46df
Dhrubot <dhrubo.dh@gmail.com>
2021-02-14 15:58:47 -0500

    successflly deployed to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDslirxqmrqGBb3-v1kCm4Ij9SPaUlsRVo',
+    authDomain: 'soc-users.firebaseapp.com',
+    projectId: 'soc-users',
+    storageBucket: 'soc-users.appspot.com',
+    messagingSenderId: '447936028667',
+    appId: '1:447936028667:web:93358c4244c9abac49355e',
+    measurementId: 'G-41LX4L9JYW',
+  },
 };
